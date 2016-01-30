87bf799d8a41df9cf509461349be8d04f504ed2c
King Wu <king.wuff101@gmail.com>
2016-01-30 15:34:53 +0800

    Upload Chapter 4 source code

---
+++ Chatper4/app.js
@@ -0,0 +1,29 @@
+'use strict';
+var messages = require('./controllers/messages');
+var compress = require('koa-compress');
+var logger = require('koa-logger');
+var serve = require('koa-static');
+var route = require('koa-route');
+var koa = require('koa');
+var path = require('path');
+var app = module.exports = koa();
+
+// Logger
+app.use(logger());
+
+app.use(route.get('/', messages.home));
+app.use(route.get('/messages', messages.list));
+app.use(route.get('/messages/:id', messages.fetch));
+app.use(route.post('/messages', messages.create));
+app.use(route.get('/async', messages.delay));
+
+// Serve static files
+app.use(serve(path.join(__dirname, 'public')));
+
+// Compress
+app.use(compress());
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('listening on port 3000');
+}
